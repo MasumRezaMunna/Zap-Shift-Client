86dfeafe89fe34413cdbe6745baae38bacce3feb
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-11-17 16:30:46 +0600

    swiper slider and design brands section

---
+++ zap-shift-client/src/pages/Brands/Brands.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import amazon from '../../assets/brands/amazon.png'
+import amazon_vector from '../../assets/brands/amazon_vector.png'
+import casio from '../../assets/brands/casio.png'
+import moonster from '../../assets/brands/moonstar.png'
+import randstad from '../../assets/brands/randstad.png'
+import star from '../../assets/brands/star.png'
+import start_people from '../../assets/brands/start_people.png'
+
+const Brands = () => {
+  return (
+    <Swiper slidesPerView={4}
+        centeredSlides={true}
+        spaceBetween={30}
+        grabCursor={true}
+        pagination={{
+          clickable: true,}}>
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Brands;
