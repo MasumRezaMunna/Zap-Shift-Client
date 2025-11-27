7292f8976692fcfdb902e139873faaed3687b711
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-11-27 09:58:34 +0600

    added parcel page

--- zap-shift-client/src/routes/router.jsx
+++ zap-shift-client/src/routes/router.jsx
@@ -7,6 +7,7 @@ import Login from "../pages/Auth/Login/Login";
 import Register from "../pages/Auth/Register/Register";
 import PrivateRoute from "./PrivateRoute";
 import Rider from "../pages/Rider/Rider";
+import SendParcel from "../pages/sendParcel/sendParcel";
 
 export const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ export const router = createBrowserRouter([
             
           </Rider>
         </PrivateRoute>
+      },
+      {
+        path: 'send-parcel',
+        element: <PrivateRoute><SendParcel></SendParcel></PrivateRoute>
       },
       {
         path: 'coverage',
